9f9269360e686fbd68991a365b06c1644a7cfc1d
allbest <allbest8989@gmail.com>
2024-11-12 17:20:39 -0500

    initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Converter from "./components/Converter";
+
+function App() {
+  return (
+    <>
+      <h1>Latitude - Longitude Converter</h1>
+      <Converter />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/config.ts
@@ -0,0 +1,7 @@
+export const API_URL = import.meta.env.VITE_API_URL;
+export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+export const TAMPA_BAY = {
+  lat: 27.9506, // Tampa Bay latitude
+  lng: -82.4572, // Tampa Bay longitude
+};
